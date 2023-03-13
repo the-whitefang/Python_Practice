2e6842cdf9173940b76eedf13857483883ca4025
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-13 16:28:28 +0530

    positional arguments

---
+++ Functions2.py
@@ -0,0 +1,9 @@
+#categories based on functions
+#based on arguments
+#1: positional arguments
+def function1(num1,num2,num3,num4):
+    print("num1",num1,"num2",num2,"num3",num3,"num4",num4)
+function1(10,20,30,40)
+function1(1,2,3,4)
+
+#2. Kewword arguments
