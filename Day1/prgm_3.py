9aa0654209bf842dd6b7432141e6105402cbd021
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-13 19:17:48 +0530

    Flight Cost Correction

--- Day1/prgm_3.py
+++ Day1/prgm_3.py
@@ -1,9 +1,11 @@
 Adult_Passengers = int(input("Enter the number of Adults: \n"))
 Child_Passengers = int(input("Enter the number of Children: \n"))
-total = ((Adult_Passengers*37750.0)+ (Child_Passengers*37550.0*0.03))
+print((((Adult_Passengers*37550.0)+(Child_Passengers*37550.0/3))*1.07)*0.90)
+
+'''total = ((Adult_Passengers*37750.0)+ (Child_Passengers*37550.0/3))
 print(total)
 total1 = total*0.07
 total2 = total1+total
 print("with service tax ",total2)
 total_amount = total2*0.90
-print(total_amount)
+print(total_amount)'''
