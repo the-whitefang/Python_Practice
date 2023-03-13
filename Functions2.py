9c61c41ca6e0a6da404c8f5d664404c534b636db
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-13 16:34:28 +0530

    keyword arguments

--- Functions2.py
+++ Functions2.py
@@ -5,5 +5,12 @@ def function1(num1,num2,num3,num4):
     print("num1",num1,"num2",num2,"num3",num3,"num4",num4)
 function1(10,20,30,40)
 function1(1,2,3,4)
+print()
 
 #2. Kewword arguments
+def function2(num1,num2,num3,num4):
+    print("num1",num1,"num2",num2,"num3",num3,"num4",num4)
+function2(num4 =20,num1=20,num3=90,num2=50)
+print()
+
+
