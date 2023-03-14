28f157428f95ffd248168b72b06ee353d0103c79
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-14 18:53:26 +0530

    program to print the equivalent swedish message

---
+++ Day3/translator.py
@@ -0,0 +1,10 @@
+def Translate(Eng_Swed,Wish):
+    for i in Wish:
+        print(Eng_Swed[i], end=' ')
+
+
+
+
+Eng_Swed = {"merry":"god","christmas":"jul","and":"soch","happy":"gott","new":"nytt","year":"ar"}
+Wish = ["merry","christmas","and","happy","new","year"]
+Translate(Eng_Swed,Wish)
