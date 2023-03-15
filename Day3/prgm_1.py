e150a9d5eba8fdf57043d497851bfc4614898409
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-15 14:41:49 +0530

    List Comprehension

--- Day3/prgm_1.py
+++ Day3/prgm_1.py
@@ -1,3 +1,25 @@
 '''
+List Comprehension
+'''
+#for loop version of printing a List
+result = []
+for i in range(11):
+    result.append(i)
+print(result)
+
+#List Comprehension version of printing a List
+print([i for i in range(11)])
+
+#for loop version --> odd elements
+l1 = []
+for i in range(11):
+    if i % 2 != 0:
+        l1.append(i)
+print(l1)
+
+#odd elements --> List Comprehension method
+print([i for i in range(11) if i %2 != 0 ])
+
+'''
 
 '''
