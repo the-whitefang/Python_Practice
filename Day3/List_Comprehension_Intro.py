d3ba17a9311121efce63dfad275e30708de1f08f
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-15 14:51:09 +0530

    List Comprehension

--- Day3/List_Comprehension_Intro.py
+++ Day3/List_Comprehension_Intro.py
@@ -20,4 +20,14 @@ print(l1)
 #odd elements --> List Comprehension method
 print([i for i in range(11) if i %2 != 0 ])
 
+#for loop version of printing if the numbers of the list is odd else if even print its square
+l1 = []
+for i in range(11):
+    if i % 2 != 0:
+        l1.append(i)
+    else:
+        l1.append(i**2)
+print(l1)
 
+#List Comprehenson version of printing if the numbers of the list is odd else if even print its square
+print([i if i % 2 != 0 else i**2 for i in range(11)])
