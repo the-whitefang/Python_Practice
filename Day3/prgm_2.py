dd7d5b4e22698213ec10fd8008796e4ffcc0f6d1
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-15 16:20:31 +0530

    List Comprehension

---
+++ Day3/prgm_2.py
@@ -0,0 +1,17 @@
+'''
+Paired outputs
+for each element in
+'''
+
+mylist = [9,3,6,1,5,0,8,2,4,7]
+list_b = [6,4,6,1,2,2]
+
+#
+result = []
+for i in list_b:
+    result.append((i,mylist.index(i)))
+print(result)
+
+#using Dictionary Comprehension
+print({i:mylist.index(i)
+      for i in list_b})
