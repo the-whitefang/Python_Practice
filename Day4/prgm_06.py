f730c97428d170e1eb4603ce57ff5d8503a5e6ed
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-16 17:23:57 +0530

    done some code

---
+++ Day4/prgm_06.py
@@ -0,0 +1,7 @@
+class Customer :
+    def  __int__(self,id):
+        self.id = id
+
+
+c1 = Customer(200)
+print(c1.id)
---
+++ Day4/prgm_07.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self):
+        self.title = None
+mav_fav = Book()
+mav_fav.title ="Head First Programming"
+your_fav = Book()
+your_fav.title = "Learn Python the Hard  way"
+
+mav_fav.title ="Learning Python"
+print("My favourite is ",mav_fav.title)
+print("Your's is ",your_fav.title)
