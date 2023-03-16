1b0fadd1ddfad8e6d8f714cf883d2a1fa7201a80
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-16 14:51:47 +0530

    program 1

--- Day4/prgm_1.py
+++ Day4/prgm_1.py
@@ -1,4 +1,25 @@
 '''
 
 '''
+import sys
+
+#regular for loop
+def nearest_palindrome(num):
+    for i in range(num+1,sys.maxsize):
+        if str(i) == str(i)[::-1]:
+            return i
+
+
+print(nearest_palindrome(1221))
+
+
+#using lambda function
+def Check_prime(n):
+    return True if (str(n)[::-1] == str(n)) else False
+
+check_prm = lambda n:str(n)[::-1]==str(n)
+n=99+1
+while(not check_prm(n)):
+    n +=1
+print(n)
 
