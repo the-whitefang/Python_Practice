14c41aa6762db97bde85ed2b3536c696a714eba3
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-16 16:26:01 +0530

    done some code

---
+++ Day4/prgm_04.py
@@ -0,0 +1,15 @@
+'''
+
+'''
+class Employee
+    def _int_(self):
+        self.employee_id = None
+        def check_eligibility(self):
+            if(self.employee_id>=9000 and self.employee_id<=10000):
+                print("The employee is eligible for special benifits")
+            else:
+                print("The employee is not eligible for special benefits")
+
+emp1 = Employee()
+emp1.employee_id =10000
+emp1.check_
