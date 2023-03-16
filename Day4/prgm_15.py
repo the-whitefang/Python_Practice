9ada84401b43c123f91f6a6154630c7fa05521f9
Abhilash <abhilashvenugopal11111@gmail.com>
2023-03-16 19:17:21 +0530

    done some code

---
+++ Day4/prgm_15.py
@@ -0,0 +1,23 @@
+class Table:
+    def __init__(self):
+        self.no_of_legs = 4
+        self.__glass_top = None
+        self.__wooden_top = None
+
+    def assign_data(self,glass_top,wooden_top):
+        self.__glass_top = glass_top
+        self.__wooden_top = wooden_top
+
+    def identity_rate(self,glass_top,wooden_top):
+        self.assign_data(glass_top,wooden_top)
+        if(self.__glass_top == True):
+            rate = 20000
+        elif (self.__wooden_top == True):
+            rate = 30000
+        else:
+            rate =0
+        return  rate
+
+dining_table = Table()
+rate = dining_table.identity_rate(False,True)
+print(rate)
