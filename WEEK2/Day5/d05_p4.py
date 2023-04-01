4aa3ef685f2654f9a12c39e332488b82f1467ffa
Abhilash <abhilashvenugopal11111@gmail.com>
2023-04-01 15:00:51 +0530

    done some code and documentation

--- WEEK2/Day5/d05_p4.py
+++ WEEK2/Day5/d05_p4.py
@@ -2,3 +2,18 @@
 Program to store Name of People their Age and their Gender and print
 all the details of the people of the same Gender as requested by the user
 '''
+
+
+class Node:
+    def __init__(self, person_name, age, gender):
+        self.person_name = person_name
+        self.age = age
+        self.gender = gender
+
+
+class People:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, person_name, age, gender):
+        pass
